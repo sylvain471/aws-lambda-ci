f6f1444a45cce1057e93ee3c8b282a75b67eee48
sylvain471 <sylvain.d@471.fr>
2024-12-07 14:53:20 +0100

    add workflows

--- src/lambda_python.py
+++ src/lambda_python.py
@@ -2,6 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+    # a first trivial example
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
