e8f3589499484ceba967177f6e852bd5789659d8
sylvain471 <sylvain.d@471.fr>
2024-12-07 15:15:25 +0100

    fix import error

--- src/lambda_python.py
+++ src/lambda_python.py
@@ -1,10 +1,14 @@
 import json
-from src.utils import get_extra_utils
+from utils import get_extra_utils
 
 def lambda_handler(event, context):
     # TODO implement
     # a first trivial example
+    print(get_extra_utils())
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+if __name__=="__main__":
+    print(lambda_handler({},{}))
